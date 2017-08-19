21647132d606459d0102723a4d18b9a41df679ad
mksri <monikait3038@gmail.com>
2017-08-19 22:29:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,82 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles ={
+'article-one': {
+	title: 'Article One | Monika Srivastava',
+	heading: 'Article one',
+	date: '15 August',
+	content: '<p>This is the content for the first article</p>'
+},
+
+'article-two':{
+	title: 'Article Two | Monika Srivastava',
+	heading: 'Article Two',
+	date: '15 August',
+	content: '<p>This is the content for the second article</p>'
+}
+};
+
+function createTemplate(data){
+	var title = data.title;
+	var heading = data.heading;
+	var date = data.date;
+	var content = data.content;
+
+
+var htmlTemplate = `
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+	<title>
+	${title}
+	</title>
+    <body>
+       <div>
+	   <a href="/">home</a>
+	   </div>
+	   </hr>
+	   <h3>
+	   ${heading}
+	   <div>
+	   ${date}
+	   </div>
+	   <div>
+	   <p>
+	   ${content}
+	   </div>
+    </body>
+</html>`;
+return htmlTemplate;
+}
+
+var names =[];
+app.get('/submit-name', function(req,res){
+	//var name = req.params.name;
+	var name = req.query.name;   //?name={value}
+	names.push(name);
+	res.send(JSON.stringify(names));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter + 1 ;
+  res.send(counter.toString());
+});
+
+app.get('/:articleName', function (req, res) {
+//articleName == article-one
+  articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -25,3 +99,4 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
