a1984ebbd53f00556e14c5bb5d2afb3b37b55142
mksri <monikait3038@gmail.com>
2017-08-30 13:13:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,24 @@ function hash(){
 app.get('/hash/:input',function(req,res){
 var hashedString = hash(req.params.input , 'this is some random string');
 res.send(hashedString);
-})
+});
+
+app.get('/create-user',function(req,res){
+var salt = crypto.getRandomBytes(128).toString('hex');
+var dbString = hash(password, salt);
+pool.query('INSERT * FROM "user"(username,password) VALUES (1$,2$)',[username,dbString], function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result.rows));
+        }
+    });
+
+});
+
+
+
 
 
 var pool = new Pool(config);
